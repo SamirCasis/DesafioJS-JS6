5dcfe6de6627c2a53953470c82077ada302323ec
samir <samir.casis@gmail.com>
2023-12-17 20:20:21 -0300

    ok con cambio de divisas pero falla al mostrar gráfico

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,70 @@
-const btnConvertir = document.querySelector('.btn');
-const inputValor = document.querySelector('input');
-const cambioP = document.querySelector('p');
+const btnConvertir = document.getElementById('btnConvertir');
+const inputMonto = document.getElementById('inputAmount');
+const resultElement = document.getElementById('result');
+const currencySelector = document.getElementById('tipodemoneda');
 const urlApi = 'https://mindicador.cl/api/';
 
 
-btnConvertir.addEventListener = ('click', () => {
-    const inputValue = inputValor.value;
-    console.log(inputValue);
-})
+const ctx = document.getElementById('myChart').getContext('2d');
+let myLineChart;
 
-const convertir = () => {
+const actualizarChart = (chartData) => {
+    if (myLineChart) {
+        myLineChart.destroy();
+    }
 
-}
+    myLineChart = new Chart(ctx, {
+        type: 'line',
+        data: chartData,
+    });
+};
+
+btnConvertir.addEventListener('click', () => {
+    const inputValue = inputMonto.value;
+    const selectedCurrency = currenrcySelecto.value;
+    convertir(inputValue, selectedCurrency);
+});
+
+const convertir = async (inputValue, selectedCurrency) => {
+    try {
+        const data = await getData();
+        const conversionRate = data[selectedCurrency.toLowerCase()].valor;
+        const result = inputValue / conversionRate;
+        pintar(result);
+
+
+        actualizarChart({
+            labels: Utils.days({ count: 10 }),
+            datasets: [{
+                label: 'My First Dataset',
+                data: [65, 59, 80, 81, 56, 55, result],
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1,
+            }],
+        });
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
 
 const getData = async () => {
-    const res = await fetch(urlApi)
-    const data = await res.json()
-    console.log(data);
-}
-
-const pintar = (data) => {
- cambioP.innerHTML += `
- <p> ${data.name} <p>`
-}
+    const res = await fetch(urlApi);
+    const data = await res.json();
+    return data;
+};
 
 const main = async () => {
-const valores = object.entries(data).map((value)) => {
-    if (typeof value [1]) !== 'string') {
-        console.log(value[1].codigo)
+    try {
+        const data = await getData();
+        console.log(data);
+    } catch (error) {
+        console.error('Error:', error);
     }
-}
-}
+};
 
-main()
+const pintar = (result) => {
+    resultElement.textContent = `Resultado: ${result.toFixed(2)}`;
+};
+
+main();
