b2244a06fa6ec92507d7fa2a189ede88e5b877af
samir <samir.casis@gmail.com>
2023-12-17 18:32:05 -0300

    ingresando funciones a JS, con fallos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,35 @@
-async function getRandomUser(){
-    const res = await fetch("https://randomuser.me/api")
+const btnConvertir = document.querySelector('.btn');
+const inputValor = document.querySelector('input');
+const cambioP = document.querySelector('p');
+const urlApi = 'https://mindicador.cl/api/';
+
+
+btnConvertir.addEventListener = ('click', () => {
+    const inputValue = inputValor.value;
+    console.log(inputValue);
+})
+
+const convertir = () => {
+
+}
+
+const getData = async () => {
+    const res = await fetch(urlApi)
     const data = await res.json()
     console.log(data);
-   }
-   getRandomUser()
+}
+
+const pintar = (data) => {
+ cambioP.innerHTML += `
+ <p> ${data.name} <p>`
+}
+
+const main = async () => {
+const valores = object.entries(data).map((value)) => {
+    if (typeof value [1]) !== 'string') {
+        console.log(value[1].codigo)
+    }
+}
+}
+
+main()
